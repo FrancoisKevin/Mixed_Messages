5c8544bd88a7ae68c3d4b601028ee06c03839ba6
FrancoisKevin <francoiskevin8@gmail.com>
2022-09-10 20:29:01 +0200

    Final

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const Horoscope () => {
-    const signes = ["Bélier","Taureau","Gémeaux","Cancer","Lion","Vierge","Balance",
-    "Scorpion","Capricorne","Verseau","Poisson"];
+function horoscope() {
+    const signes = ["Bélier: ","Taureau: ","Gémeaux: ","Cancer: ","Lion: ","Vierge: ","Balance: ",
+    "Scorpion: ","Capricorne: ","Verseau: ","Poisson: "];
     const journee = ["Une belle journée en prévision!","Faites attention aux autre, vous vous sentirez mieux !",
 "Vénus est de votre côté!","Uranus est dans l'anus, restez chez vous aujourd'hui.","Neptune t'encule.",
 "Mercure a besoin d'une petite cure, ne comptez pas sur ses bienfaits pour cette journée.",
@@ -9,9 +9,11 @@ const Horoscope () => {
 "Ce signe a-t-il vraiment besoin d'un horoscope ? Ta journée sera merdique."];
 
 const phraseFinale = signes[Math.floor(Math.random()* signes.length)];
+const finDePhraseFinale = journee[Math.floor(Math.random()* journee.length)];
 
-console.log(phraseFinale);
+console.log(phraseFinale+finDePhraseFinale);
+return phraseFinale+finDePhraseFinale;
 
 }
 
-Horoscope();
+horoscope();
